520a95d28f4984a83b4be87c7c5d73de67ae9d6b
Rushikesh Kale <rushikeshkale509@gmail.com>
2026-06-04 21:23:48 +0530

    Basic python programs

---
+++ new3.py
@@ -0,0 +1,103 @@
+'''a = 10
+b = 20
+a,b = b,a
+print("a:-",a)
+print("b:-",b)'''
+
+'''a = input("enter a 1st no")
+b = input("enter a 2nd no")
+print("a<<b",a<<b)'''
+
+'''a = 2+3j
+b = 5+4j
+
+print("a+b is ",a+b)
+print("a-b is ",a-b)
+print("a*b is ",a*b)
+print("a/b is ",a/b)'''
+
+'''c = 10
+d = 10
+print(id(c))
+print(id(d))
+print(c is d)'''
+
+
+'''a = 'A'
+print(ord(a))
+
+b = 65
+print(chr(b))'''
+
+
+'''a= "hello"
+print(('h' in a))
+print(('H' in a))'''
+
+
+'''a=input("Enter A Name: ")
+b=input("Enter Char To Check: ")
+print(b in a)'''
+
+
+'''a = int(input("enter a no : "))
+if (a%5==0) :
+    print("given no is multiply by 5")
+else :
+    print("not multiply by 5")'''
+    
+
+
+'''a = int(input("enter a no"))
+if(a>=0):
+    print("positive")
+else:
+    print("negative")'''
+    
+    
+    
+'''a = int(input("enter selling price "))
+b = input ("actual cost ")
+if(a>=b):
+    print("profit ")
+else:
+    print("loss ")'''
+    
+    
+    
+'''a=int(input("enter a length "))
+b=input("enter a breadth ")
+if (a==b):
+    print("its a SQUARE")
+else:
+    print("its a RECTANGLE")'''
+    
+
+'''a=int(input("enter a no "))
+
+if(a%3==0 or a%5==0):
+    print("muliply by 3 or 5")
+else:
+    print("not muliply by 3 or 5")'''
+    
+    
+'''print("1 -",1*1)
+print("2 -",2*2)
+print("3 -",3*3)
+print("4 -",4*4)
+print("5 -",5*5)'''
+
+'''a=int(input("enter 1st number"))
+b=int(input("enter 2nd number"))
+c=int(input("enter 3rd number"))
+a=a*2
+b=b*2
+c=c*2
+d=(a+b+c)/3
+print(a)
+print(b)
+print(c)
+print(d)'''
+
+'''a= int(input("enter dollar"))
+print("dollar in rupees ",a*97)'''
