5f964f7c49065918b65ead80a38bf543fae8be77
Rushikesh Kale <rushikeshkale509@gmail.com>
2026-06-05 13:00:14 +0530

    while loop

---
+++ fri.py
@@ -0,0 +1,71 @@
+'''Wap to check Enter number is multiple 7 and 3
+ using short hand if else'''
+#a = int(input("enter a to check  "))
+#print("multiple of 3 & 7") if a%3==0 and a%7==0 else print("not multiple of 3 & 7")
+
+'''Wap to print first n natural numbers in 
+depending order get n from user'''
+
+'''n = int(input("enter a number  "))
+while n>0 :
+    print(n ,end=" ")
+    n-=1
+'''
+
+
+'''Wap to print 2s table in following 
+format 2×1=2'''
+
+'''a = 1
+while a<=10:
+    print("2 *",a,"=",2*a)
+   # print(f"2*{a}= {2*a}")
+    a+=1
+'''
+
+'''Wap to print even natural numbers upto 100'''
+
+'''a = 2
+while a<=100:
+    print(a)
+    a+=2
+'''
+
+'''Wap print addition of 1st n natural numbers'''
+
+'''a=int(input("enter a number "))
+sum = 0
+while a>=1:
+    sum=sum+a
+   # print("Addition ",sum)
+    a-=1
+
+print("Addition ",sum)'''
+
+'''Wap print addition of even number upto
+ n natural numbers'''
+
+'''a=int(input("enter a number "))
+sum = 0
+while a>=1:
+    if a%2==0:
+        sum=sum+a
+    a-=1
+
+print("Addition ",sum)'''
+
+'''Wap to print a to z'''
+
+'''ch = "Aa"
+while ch<="Zz":
+    print(ch)
+    ch=chr(ord(ch)+1)
+'''
+
+a=100
+while a<=500:
+    if a%13==0 and a%3!=0:
+        print(a)
+    a+=1
+
+
