05e8706f09d49c4c8efa669142b41c83f2acd4b7
Rushikesh Kale <rushikeshkale509@gmail.com>
2026-06-09 12:27:53 +0530

    Pattern's Using Nested for Loop

---
+++ tue9june.py
@@ -0,0 +1,148 @@
+'''for i in range (4):
+    for j in range (i+1):
+        print("*",end="")
+    print()'''
+
+
+
+'''for i in range (1,5):
+    for j in range (1,i+1):
+        print(j,end="")
+    print()'''
+    
+
+'''for i in range (1,5):
+    for j in range (1,i+1):
+        print(i,end="")
+    print()'''
+    
+
+
+'''for i in range (4):
+    for j in range (4,i,-1):
+        print("*",end="")
+    print()'''
+    
+                #OR
+    
+'''for i in range (4,0,-1):
+    for j in range (i,0,-1):
+        print("*",end="")
+    print()'''
+    
+    
+    
+'''for i in range (1,5):
+    for j in range (5,i,-1):
+        print(i,end="")
+    print()'''
+    
+    
+'''for i in range (4,0,-1):
+    for j in range (1,i+1):
+        print(j,end="")
+    print()'''
+    
+'''n=1 
+for i in range (1,5):
+    for j in range (1,i+1):
+        print(n,end="")
+        n+=1
+    print()'''
+    
+
+
+'''n='A' 
+for i in range (1,5):
+    for j in range (1,i+1):
+        print(n,end="")
+    print()
+    n=chr(ord(n)+1)'''
+    
+    
+'''n='A' 
+for i in range (1,5):
+    for j in range (1,i+1):
+        print(n,end="")
+        n=chr(ord(n)+1)
+    print()'''
+    
+
+    
+'''n='C'   
+for i in range (1,4):
+    for j in range (4,i,-1):
+        print(n,end="")
+    print()
+    n=chr(ord(n)-1)'''
+    
+    
+'''n=int(input("enter a num "))   
+for i in range (1,n+1):
+    for k in range (n-i):
+        print(" ",end="")
+    for j in range (i):
+        print("*",end="")
+    print()'''
+    
+    
+'''n=4
+for i in range (n,0,-1):
+    for k in range (n-i):
+        print(" ",end="")
+    for j in range (i):
+        print("*",end="")
+    print()'''
+    
+            #OR
+
+
+'''n=4
+for i in range (n):
+    for k in range (i):
+        print(" ",end="")
+    for j in range (n-i):
+        print("*",end="")
+    print()'''
+    
+
+
+
+    
+'''n=5
+for i in range (1,n+1):
+    for k in range (n-i):
+        print(" ",end="")
+    for j in range (2*i-1):
+        print("*",end="")
+    print()''' 
+    
+
+
+'''n=5
+for i in range (n,0,-1):
+    for k in range (n-i):
+        print(" ",end="")
+    for j in range (2*i-1):
+        print("*",end="")
+    print()'''
+    
+    
+'''n=5
+for i in range (1,n+1):
+    for k in range (n-i):
+        print(" ",end="")
+    for j in range (2*i-1):
+        print("*",end="")
+    print()
+    
+for i in range (n-1,0,-1):
+    for k in range (n-i):
+        print(" ",end="")
+    for j in range (2*i-1):
+        print("*",end="")
+    print()'''
+    
+    
+    
+
